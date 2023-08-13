faccee8bc0b4d34fef138699a3aa932a209e7d6d
Morsalin3 <mdmorsalin3@gmail.com>
2023-08-13 21:25:31 +0600

    create card post and get api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_PASS);
+// console.log(process.env.DB_PASS);
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ojgvcnk.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,12 +29,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection = client.db('sellHub').collection('products');
+    const cardsCollection = client.db('sellHub').collection('cards')
+
+
+    app.get('/products', async(req, res)=>{
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/product/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId (id)};
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+
+    });
+
+    app.post('/card', async(req, res) =>{
+      const card = req.body;
+      const result = await cardsCollection.insertOne(card);
+      res.send(result); 
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
